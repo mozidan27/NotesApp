7f156a46351ae461ef87c703864bbe51bc4fca4e
mozidan27 <153581941+mozidan27@users.noreply.github.com>
2024-05-07 08:57:34 +0300

    bulit the text field and floating AB but the delete and edit icons not added yet

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:notes/models/note.dart';
+import 'package:notes/models/note_database.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // text controller to access what the user typed
+
+  final textController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+
+    // on app startup , fetch existing notes
+    readNote();
+  }
+  // create note
+
+  void createNote() {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: TextField(
+          controller: textController,
+        ),
+        actions: [
+          // create button
+          MaterialButton(
+            onPressed: () {
+              // add note to firebase
+              context.read<NoteDatabase>().addNote(textController.text);
+
+              //clear controller
+              textController.clear();
+              // close dialog
+
+              Navigator.of(context).pop();
+            },
+            child: const Text('Create'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  // read note
+
+  void readNote() {
+    context.read<NoteDatabase>().fetchNotes();
+  }
+
+  // update note
+
+  // delete ntoe
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    // note db
+
+    final noteDataBase = context.watch<NoteDatabase>();
+    // list of notes
+    List<Note> currentNote = noteDataBase.currentNNotes;
+    return Scaffold(
+      backgroundColor: Colors.grey.shade300,
+      appBar: AppBar(
+        title: const Text(
+          "Notes",
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold, fontSize: 32),
+        ),
+        backgroundColor: Colors.grey.shade800,
+        elevation: 3,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNote,
+        child: const Icon(Icons.add),
+      ),
+      body: ListView.builder(
+        itemCount: currentNote.length,
+        itemBuilder: (context, index) {
+          // get individual note
+
+          final note = currentNote[index];
+
+          // list tile ui
+
+          return ListTile(
+            title: Text(note.text),
+          );
+        },
+      ),
+    );
   }
 }
